17e63bb7cad116c4e7c1d39a6da5826f4bfc9a24
Alvin <AlvinWang627@gmail.com>
2022-02-14 21:19:47 +0800

    feat: revise addBtn minusBtn subtotal freight logic

--- src/components/shoppingCart.vue
+++ src/components/shoppingCart.vue
@@ -4,7 +4,7 @@
     <div class="product-content">
       <div
         class="product-content_item"
-        v-for="(product, index) in products"
+        v-for="product in products"
         :key="product.id"
       >
         <img :src="product.image" alt="" />
@@ -13,15 +13,12 @@
           <div class="quantity-control">
             <button
               class="btn-minus"
-              @click.stop.prevent="minusBtn(index), saveStorage(index)"
+              @click.stop.prevent="minusBtn(product.id)"
             >
               –
             </button>
             <div class="quantities">{{ product.quantity }}</div>
-            <button
-              class="btn-add"
-              @click.stop.prevent="addBtn(index), saveStorage(index)"
-            >
+            <button class="btn-add" @click.stop.prevent="addBtn(product.id)">
               ＋
             </button>
           </div>
@@ -72,36 +69,32 @@ export default {
     };
   },
   filters: {
+    //將金額增加千分位","
     formatPrice(price) {
       return `$${price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
     },
   },
   methods: {
     //購物車加號按鈕
-    addBtn(index) {
-      // 數量+1
-      this.products[index].quantity += 1;
-      //讓右邊商品金額同步更新
-      if (this.products[index].name === "破壞補丁修正牛仔褲") {
-        this.products[index].price = this.products[index].quantity * 3999;
-      } else {
-        this.products[index].price = this.products[index].quantity * 1299;
-      }
+    addBtn(id) {
+      const updateProducts = this.products.map((product) => {
+        if (product.id === id) {
+          return { ...product, quantity: (product.quantity += 1) };
+        }
+        return product;
+      });
+
+      this.products = updateProducts;
     },
     //購物車減號按鈕
-    minusBtn(index) {
-      //預防變成負的
-      if (this.products[index].quantity === 0) {
-        return;
-      }
-      //數量-1
-      this.products[index].quantity -= 1;
-      //讓右邊商品金額同步更新
-      if (this.products[index].name === "破壞補丁修正牛仔褲") {
-        this.products[index].price = this.products[index].quantity * 3999;
-      } else {
-        this.products[index].price = this.products[index].quantity * 1299;
-      }
+    minusBtn(id) {
+      const updateProducts = this.products.map((product) => {
+        if (product.id === id) {
+          return { ...product, quantity: (product.quantity -= 1) };
+        }
+        return product;
+      });
+      this.products = updateProducts;
     },
     fetchFreight() {
       this.freight = this.initialShoppingcartcontent.freight;
@@ -126,6 +119,15 @@ export default {
       const { freight } = this.initialShoppingcartcontent;
       return { products, freight };
     },
+    subTotal2() {
+      //小計總額計算用，商品加上運費，丟到watch做監控
+      const currentFreight =
+        this.initialShoppingcartcontent.freight === "免費" ? 0 : 500;
+      const productsPrice = this.products.reduce((acc, product) => {
+        return (acc += product.quantity * product.price);
+      }, 0);
+      return currentFreight + productsPrice;
+    },
   },
   created() {
     this.handleSubtotal();
@@ -134,15 +136,7 @@ export default {
     //小計更新用，金額有變就更新
     listenChange: {
       handler: function () {
-        if (this.initialShoppingcartcontent.freight === "免費") {
-          this.subTotal =
-            parseInt(this.products[0].price) + parseInt(this.products[1].price);
-        } else {
-          this.subTotal =
-            parseInt(this.products[0].price) +
-            parseInt(this.products[1].price) +
-            500;
-        }
+        this.subTotal = this.subTotal2;
       },
       deep: true,
     },
